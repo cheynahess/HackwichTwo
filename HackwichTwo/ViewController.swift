11681d13772b4128d7eff143dd17f83af516bb34
Cheyna Hess <cheynah@hawaii.edu>
2022-02-03 11:23:28 -1000

    Completed parts 1-8 of Hackwich2.

--- HackwichTwo/ViewController.swift
+++ HackwichTwo/ViewController.swift
@@ -14,22 +14,17 @@ class ViewController: UIViewController {
         //this is the code block for viewDidLoad
         // Do any additional setup after loading the view.
     }
-
+    
     
     @IBAction func PressMeButtonPressed(_ sender: Any) {
-    
-    print("Hello World")
-    
-    }
-    
-    
-    
-    
-    
-    
-    
-    
-    
+        
+  print("HelloWorld")}
 
+
+    @IBAction func SecondButtonPressed(_ sender: Any) {
+        print("I Got This")
+}
+    
+    
 }
 
